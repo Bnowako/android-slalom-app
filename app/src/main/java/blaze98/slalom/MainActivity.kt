458d7f51544c6953a2e3cc5965af43f9cfe685f9
Bnowako <bnowakowski98@gmail.com>
2022-05-28 17:34:39 +0200

    create instance of FusedLocationProviderClient

--- app/src/main/java/blaze98/slalom/MainActivity.kt
+++ app/src/main/java/blaze98/slalom/MainActivity.kt
@@ -2,10 +2,18 @@ package blaze98.slalom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 }
