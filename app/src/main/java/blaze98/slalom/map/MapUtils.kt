89a8daa515923994deee1354fb89810c396dba3d
Bnowako <bnowakowski98@gmail.com>
2022-05-29 17:11:40 +0200

    add functions that handle placing polygons on map, to show area of monster

--- app/src/main/java/blaze98/slalom/map/MapUtils.kt
+++ app/src/main/java/blaze98/slalom/map/MapUtils.kt
@@ -3,9 +3,33 @@ package blaze98.slalom.map
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polygon
+import com.google.android.gms.maps.model.PolygonOptions
 
-class MapUtils(val map: GoogleMap) {
+class MapUtils(
+    private val map: GoogleMap, private val latOffset: Double = 0.001, private val lonOffset: Double = 0.0014
+) {
 
+
+    private fun placeRectangle(latLng: LatLng): Polygon {
+        return map.addPolygon(
+            PolygonOptions()
+                .clickable(true)
+                .add(
+                    LatLng(latLng.latitude + latOffset, latLng.longitude - lonOffset),
+                    LatLng(latLng.latitude + latOffset, latLng.longitude + lonOffset),
+                    LatLng(latLng.latitude - latOffset, latLng.longitude + lonOffset),
+                    LatLng(latLng.latitude - latOffset, latLng.longitude - lonOffset),
+                    LatLng(latLng.latitude + latOffset, latLng.longitude - lonOffset)
+                )
+        )
+    }
+
+    fun placePolygons(locations: MutableList<LatLng>): List<Polygon> {
+        return locations.map {
+            placeRectangle(it)
+        }
+    }
 
     fun placeMarkers(locations: MutableList<LatLng>) {
         locations.forEach {
@@ -13,7 +37,7 @@ class MapUtils(val map: GoogleMap) {
         }
     }
 
-    fun addMarker(location: LatLng) {
+    private fun addMarker(location: LatLng) {
         map.addMarker(
             MarkerOptions()
                 .position(location)
