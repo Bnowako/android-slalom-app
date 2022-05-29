1a6ece89da794748ee2d94638788b3fb4b573a74
Bnowako <bnowakowski98@gmail.com>
2022-05-29 13:19:34 +0200

    setup localization in MainActivity

--- app/src/main/java/blaze98/slalom/MainActivity.kt
+++ app/src/main/java/blaze98/slalom/MainActivity.kt
@@ -1,19 +1,48 @@
 package blaze98.slalom
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        setupLocalization()
 
+    }
+
+    private fun setupLocalization() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), 12)
+            locationManager = getSystemService(Context.LOCATION_SERVICE)as LocationManager
+            val provider = locationManager.getBestProvider(Criteria(), true)
+            if(provider != null) {
+                val listener = prepareLocationListener()
+                locationManager.requestLocationUpdates(provider, 0L, 0f, listener)
+            }
+        }
+    }
+
+    private fun prepareLocationListener() = LocationListener {
+        println(" ")
+        println(it.latitude)
+        println(it.longitude)
+        println(" ")
     }
 }
