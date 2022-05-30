0496e7a843bf67823222adafbcb5ba5f8803173f
Bnowako <bnowakowski98@gmail.com>
2022-05-30 18:04:24 +0200

    add function to create finish area

--- app/src/main/java/blaze98/slalom/map/MapUtils.kt
+++ app/src/main/java/blaze98/slalom/map/MapUtils.kt
@@ -4,11 +4,31 @@ import android.content.Context
 import android.graphics.Color
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.*
+import kotlin.random.Random
 
 class MapUtils(
     private val map: GoogleMap, private val context: Context, private val latOffset: Double = 0.001, private val lonOffset: Double = 0.0014
 ) {
 
+    fun placeFinishArea(userLatLng: LatLng): Polygon {
+        val randomCoefficient1 = if (Random.nextBoolean()) -1 else 1
+        val randomCoefficient2 = if (Random.nextBoolean()) -1 else 1
+        val finishArea = LatLng(userLatLng.latitude + 0.012 * randomCoefficient1, userLatLng.longitude + 0.015 * randomCoefficient2)
+        return map.addPolygon(
+            PolygonOptions()
+                .clickable(true)
+                .add(
+                    LatLng(finishArea.latitude + latOffset, finishArea.longitude - lonOffset),
+                    LatLng(finishArea.latitude + latOffset, finishArea.longitude + lonOffset),
+                    LatLng(finishArea.latitude - latOffset, finishArea.longitude + lonOffset),
+                    LatLng(finishArea.latitude - latOffset, finishArea.longitude - lonOffset),
+                    LatLng(finishArea.latitude + latOffset, finishArea.longitude - lonOffset)
+                )
+                .strokeColor(Color.parseColor("#fc0303"))
+                .strokeWidth(2f)
+                .fillColor(Color.argb(20, 50, 255, 100))
+        )
+    }
 
     private fun placeRectangle(latLng: LatLng): Polygon {
         return map.addPolygon(
@@ -23,7 +43,7 @@ class MapUtils(
                 )
                 .strokeColor(Color.parseColor("#fc0303"))
                 .strokeWidth(2f)
-                .fillColor(Color.argb(20,50,0,255))
+                .fillColor(Color.argb(20, 50, 0, 255))
         )
     }
 
