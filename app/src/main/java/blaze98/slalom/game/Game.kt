0a188b14491fd8b485e7818d272c896f92c57370
Bnowako <bnowakowski98@gmail.com>
2022-05-30 17:28:16 +0200

    add task that makes monsters sleep/awake

--- app/src/main/java/blaze98/slalom/game/Game.kt
+++ app/src/main/java/blaze98/slalom/game/Game.kt
@@ -2,6 +2,9 @@ package blaze98.slalom.game
 
 import android.content.Context
 import android.location.Location
+import android.os.Handler
+import android.os.Looper
+import blaze98.slalom.R
 import blaze98.slalom.map.MapUtils
 import blaze98.slalom.monster.MonsterFabric
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -10,11 +13,14 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Polygon
 import com.google.maps.android.PolyUtil
 
+
 class Game(
     var mMap: GoogleMap
 ) {
     private lateinit var mapUtils: MapUtils
     private var allMonsters: MutableList<Polygon> = mutableListOf()
+    private var monstersAwake: Boolean = true
+    val mainHandler = Handler(Looper.getMainLooper())
 
 
     fun init(location: Location, context: Context) {
@@ -25,18 +31,30 @@ class Game(
 
         val userLatLng = LatLng(location.latitude, location.longitude)
         mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLng))
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                monstersAwake = !monstersAwake
+                val style = if (monstersAwake) R.raw.light_map else R.raw.dark_map
+                mapUtils.changeMapStyle(style)
+                mainHandler.postDelayed(this, 3000)
+            }
+        })
+
     }
 
     fun isUserAlive(currLocation: Location): Boolean {
-        var userAlive = true
-        val latLng = LatLng(currLocation.latitude, currLocation.longitude)
-        allMonsters.forEach {
-            if(PolyUtil.containsLocation(latLng, it.points,true)) {
-                userAlive = false
-                return@forEach
+        if(monstersAwake) {
+            var userAlive = true
+            val latLng = LatLng(currLocation.latitude, currLocation.longitude)
+            allMonsters.forEach {
+                if (PolyUtil.containsLocation(latLng, it.points, true)) {
+                    userAlive = false
+                    return@forEach
+                }
             }
+            return userAlive
         }
-        return userAlive
+        return true
     }
 
 }
