f4e23cf4d2e1e1d309625d376768f9a55435ad01
Bnowako <bnowakowski98@gmail.com>
2022-05-30 20:49:34 +0200

    add monster every 2 iterations

--- app/src/main/java/blaze98/slalom/game/Game.kt
+++ app/src/main/java/blaze98/slalom/game/Game.kt
@@ -7,6 +7,7 @@ import android.os.Looper
 import blaze98.slalom.R
 import blaze98.slalom.map.MapUtils
 import blaze98.slalom.monster.MonsterFabric
+import blaze98.slalom.monster.MonsterFabric.Companion.getMonsterLocation
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
@@ -20,11 +21,14 @@ class Game(
     private lateinit var mapUtils: MapUtils
     private var allMonsters: MutableList<Polygon> = mutableListOf()
     private var monstersAwake: Boolean = true
+    private var iteration: Int = 0
+    private lateinit var lastLocation: Location
     val mainHandler = Handler(Looper.getMainLooper())
     private lateinit var finishArea: Polygon
 
 
     fun init(location: Location, context: Context) {
+        lastLocation = location
         val monsters = MonsterFabric.getNMonstersLocations(10, location)
         mapUtils = MapUtils(mMap, context)
         val polygons = mapUtils.placePolygons(monsters)
@@ -35,15 +39,18 @@ class Game(
         mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLng))
         mainHandler.post(object : Runnable {
             override fun run() {
+                iteration += 1
+                if(iteration % 2 == 0) addMonster(lastLocation)
                 monstersAwake = !monstersAwake
                 val style = if (monstersAwake) R.raw.dark_map else R.raw.light_map
                 mapUtils.changeMapStyle(style)
-                mainHandler.postDelayed(this, 7000)
+                mainHandler.postDelayed(this, 5000)
             }
         })
     }
 
     fun isUserAlive(currLocation: Location): Boolean {
+        lastLocation = currLocation
         if(monstersAwake) {
             var userAlive = true
             val latLng = LatLng(currLocation.latitude, currLocation.longitude)
@@ -58,4 +65,9 @@ class Game(
         return true
     }
 
+    fun addMonster(currLocation: Location) {
+        val monsterLocation = getMonsterLocation(currLocation)
+        val monsterPolygon = mapUtils.placeRectangle(monsterLocation)
+        allMonsters.add(monsterPolygon)
+    }
 }
