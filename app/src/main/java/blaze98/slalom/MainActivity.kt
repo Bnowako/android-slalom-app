c53fac52fef72fe3f325ce8a53156de3db826499
Bnowako <bnowakowski98@gmail.com>
2022-05-31 17:14:45 +0200

    add displaying toast after user starts game

--- app/src/main/java/blaze98/slalom/MainActivity.kt
+++ app/src/main/java/blaze98/slalom/MainActivity.kt
@@ -1,19 +1,32 @@
 package blaze98.slalom
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnStartGame.setOnClickListener {
             val intent = Intent(this, MapGame::class.java)
+            showToastWithText("Game is starting..")
             startActivity(intent)
+
         }
+    }
+
+
+    private fun showToastWithText(text: String) {
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
     }
 
 }
