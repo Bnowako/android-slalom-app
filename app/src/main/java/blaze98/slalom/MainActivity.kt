bc1a25e2dfa33ec615dde470a351383c195b9701
Bnowako <bnowakowski98@gmail.com>
2022-05-31 16:47:22 +0200

    add button that displays MapGame activity

--- app/src/main/java/blaze98/slalom/MainActivity.kt
+++ app/src/main/java/blaze98/slalom/MainActivity.kt
@@ -1,13 +1,19 @@
 package blaze98.slalom
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnStartGame.setOnClickListener {
+            val intent = Intent(this, MapGame::class.java)
+            startActivity(intent)
+        }
     }
 
 }
